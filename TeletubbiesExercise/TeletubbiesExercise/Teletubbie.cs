fb60c6914cce82355ff781618f6a26785a275f69
Aleksandra Bruska <s185454@student.pg.edu.pl>
2024-10-05 14:04:37 +0200

    Week 6 exercise 1 done

---
+++ TeletubbiesExercise/TeletubbiesExercise/Teletubbie.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeletubbiesExercise
+{
+    internal class Teletubbie: IObserver
+    {
+        public void Update(DayPart dayPart)
+        {
+            switch (dayPart)
+            {
+                case DayPart.MORNING: WakeUp();
+                    break;
+                case DayPart.AFTERNOON: WatchTv();
+                    break;
+                case DayPart.EVENING: EatDinner();
+                    break;
+                case DayPart.NIGHT: ByeBye();
+                    break;
+                default: 
+                    break;
+            }
+        }
+        public void EatDinner()
+        {
+            Console.WriteLine("I am eating dinner omnom");
+        }
+        public void WatchTv()
+        {
+            Console.WriteLine("I am watching TV ....");
+        }
+        public void WakeUp()
+        {
+            Console.WriteLine("I am up yawn");
+        }
+        public void ByeBye()
+        {
+            Console.WriteLine("Bye bye");
+        }
+
+    }
+}
---
+++ TeletubbiesExercise/TeletubbiesExercise/main.cs
@@ -0,0 +1,27 @@
+﻿using TeletubbiesExercise;
+
+class MainClass
+{
+    public static void Main()
+    {
+        Teletubbie tinkyWinky = new Teletubbie();
+        Teletubbie lala = new Teletubbie();
+        Teletubbie po = new Teletubbie();
+        Teletubbie dipsy = new Teletubbie();
+        Telephone telephone = new Telephone();
+        Teletubbie[] allOfThem = {tinkyWinky, lala, dipsy, po};
+        foreach(Teletubbie teletubbie in allOfThem)
+        {
+            telephone.Attach(teletubbie);
+        }
+        telephone.Notify();
+        telephone.SetTime(DayPart.AFTERNOON);
+        telephone.Notify();
+        telephone.Detach(dipsy);
+        telephone.SetTime(DayPart.EVENING);
+        telephone.Notify();
+        telephone.Attach(dipsy);
+        telephone.SetTime(DayPart.NIGHT);
+        telephone.Notify();
+    }
+}
