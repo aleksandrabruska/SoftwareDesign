74d835e61b748a578ddffc63e47e39a43765f768
Aleksandra Bruska <s185454@student.pg.edu.pl>
2024-10-06 18:04:10 +0200

    Stock Exercise done

---
+++ ObserverPattern/StocksExercise/Portfolio.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StocksExercise
+{
+  
+    internal class Portfolio : IObserver
+    {
+        private float totalValue;
+        private PortfolioDisplay display;
+        private List<(Stock, float, float)> myStocks; // stock amount total value
+        
+        public Portfolio()
+        {
+            myStocks = new List<(Stock, float, float)>();
+            display = new PortfolioDisplay();
+        }
+        public void Update()
+        {
+            for (int i = 0; i < myStocks.Count; i ++)
+            {
+                float stockValue = myStocks[i].Item1.getValue();
+                myStocks[i] = (myStocks[i].Item1, myStocks[i].Item2, stockValue * myStocks[i].Item2) ;
+                //Console.WriteLine(myStocks[i].Item3);
+            }
+            calculateTotalValue();
+            display.display(myStocks);
+        }
+
+        private void calculateTotalValue()
+        {
+            totalValue = 0;
+            foreach (var item in myStocks)
+            {
+                totalValue += item.Item3 * item.Item2;
+            }
+        }
+
+        public void addStock(Stock stock, float amount)
+        {
+            myStocks.Add((stock, amount, -1.0f));
+            //Update();
+        }
+    }
+}
---
+++ ObserverPattern/StocksExercise/Program.cs
@@ -0,0 +1,52 @@
+﻿
+namespace StocksExercise
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            Stock google = new Stock("Google", 10.0f);
+            Stock microsoft = new Stock("Microsoft", 15.0f);
+            Stock apple = new Stock("Apple", 1.5f);
+            Stock intel = new Stock("Intel", 0.0001f);
+
+            Portfolio[] portfolios = new Portfolio[3];
+
+            portfolios[0] = new Portfolio();
+            portfolios[1] = new Portfolio();
+            portfolios[2] = new Portfolio();
+
+            google.Attach(portfolios[0]);
+            google.Attach(portfolios[1]);
+
+            microsoft.Attach(portfolios[0]);
+            microsoft.Attach(portfolios[2]);
+
+            apple.Attach(portfolios[1]);
+            apple.Attach(portfolios[2]);
+
+            intel.Attach(portfolios[0]);
+            intel.Attach(portfolios[1]);
+            intel.Attach(portfolios[2]);
+
+            portfolios[0].addStock(google, 1.2f);
+            portfolios[0].addStock(microsoft, 3.5f);
+            portfolios[0].addStock(intel, 0.7f);
+            
+            portfolios[1].addStock(google, 3.7f);
+            portfolios[1].addStock(apple, 1.4f);
+            portfolios[1].addStock(intel, 0.9f);
+            
+            portfolios[2].addStock(microsoft, 0.3f);
+            portfolios[2].addStock(apple, 5.7f);
+            portfolios[2].addStock(intel, 3.7f);
+
+
+            intel.setValue(0.5f);
+
+            Console.WriteLine("\n\n CHANGED \n");
+            intel.setValue(3.0f);
+        }
+    
+    }
+}
