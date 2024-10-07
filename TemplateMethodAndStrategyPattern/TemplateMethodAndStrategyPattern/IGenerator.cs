456db2bc4caf67e1cd6a433afef84535d48d6723
Aleksandra Bruska <s185454@student.pg.edu.pl>
2024-10-07 13:57:34 +0200

    Strategy pattern exercise

---
+++ TemplateMethodAndStrategyPattern/TemplateMethodAndStrategyPattern/IGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodAndStrategyPattern
+{
+    internal interface IGenerator
+    {
+        public int[] generate(uint size, int seed);
+    }
+
+}
---
+++ TemplateMethodAndStrategyPattern/TemplateMethodAndStrategyPattern/ReverseOrder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodAndStrategyPattern
+{
+    internal class ReverseOrder : IGenerator
+    {
+        public int[] generate(uint size, int seed)
+        {
+            Random random = new Random(seed);
+            int[] result = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                if (i == 0)
+                {
+                    result[i] = random.Next();
+                }
+                else
+                {
+                    result[i] = random.Next(result[i - 1]);
+                }
+            }
+          
+            return result;
+        }
+    }
+}
