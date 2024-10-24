4a303e18b2d91905f9d9163d9c1cad9fe67d0f27
Aleksandra Bruska <s185454@student.pg.edu.pl>
2024-10-24 09:58:29 +0200

    Week 8 part 2 started

---
+++ TheStatePattern/Telephone/Phone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telephone
+{
+    internal class Phone
+    {
+        PhoneState state;
+        public void SetState(PhoneState state)
+        {
+            this.state = state;
+        }
+        public void Idle()
+        {
+            Console.WriteLine("I am idle");
+        }
+        public void Busy()
+        {
+            Console.WriteLine("Busy!");
+        }
+        public void Call()
+        {
+            Console.WriteLine("I am calling");
+        }
+        public void Connect()
+        {
+            Console.WriteLine("I am connected");
+        }
+
+        public void Disconect()
+        {
+            Console.WriteLine("I am disconected");
+        }
+    }
+}
