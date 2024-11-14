2143f4f992b306ecd8565b43608b8f20c32fe399
aleksandrabruska <aleksandrabr405@gmail.com>
2024-11-14 09:17:53 +0100

    Week 11

---
+++ ParallelLoops/ParallelLoops/NaiveParallelFor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelLoops
+{
+    internal class NaiveParallelFor
+    {
+        public static void MyParallelFor(int inclusiveLowerBound, int exclusiveUpperBound, Action<int> body)
+        {
+            // Determine size of each partition of work (size/nCores) – static partitioning
+            int size = exclusiveUpperBound - inclusiveLowerBound;
+            int numProcs = Environment.ProcessorCount;
+            int range = size / numProcs;
+
+            // Initialize threads to do work
+            var threads = new List<Thread>(numProcs);
+            for (int p = 0; p < numProcs; p++)
+            {
+                int start = p * range + inclusiveLowerBound;
+                int end = (p == numProcs - 1) ? exclusiveUpperBound : start + range;
+                threads.Add(new Thread(() => {
+                    for (int i = start; i < end; i++) body(i);
+                }));
+            }
+
+            // Start and await threads
+            foreach (var thread in threads) thread.Start();  // Start them all
+            foreach (var thread in threads) thread.Join();   // wait on all
+        }
+    }
+}
---
+++ ParallelLoops/ParallelLoops/Program.cs
@@ -0,0 +1,90 @@
+﻿using ParallelLoops;
+using System.Diagnostics;
+public struct abc {
+    public double A;
+    public double B;
+    public double C;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        const int N = 80000000;
+        double[] A, B, C;
+        A = new double[N];
+        B = new double[N];
+        C = new double[N];
+
+        Random rand = new Random();
+        for (int i = 0; i < N; i++)
+        {
+            A[i] = rand.Next();
+            B[i] = rand.Next();
+            C[i] = rand.Next();
+        }
+
+        Console.WriteLine("Starts sequential for now.");
+        stopwatch.Start();
+        for (int i = 0; i < N; i++)
+        {
+            C[i] = A[i] * B[i] - A[i] * B[i] + A[i] + A[i] * B[i] - A[i] / A[i] + A[i] / B[i];
+            C[i] = A[i] * B[i] - A[i] * B[i] + A[i];
+            C[i] = A[i] * B[i] - A[i] + 1 - 3 + A[i] / B[i];
+            C[i] = A[i] * B[i] - A[i] * B[i] + A[i];
+        }
+        stopwatch.Stop();
+        Console.WriteLine("Sequential loop time in milliseconds: {0}",
+                          stopwatch.ElapsedMilliseconds);
+        stopwatch.Reset();
+
+        stopwatch.Start();
+        NaiveParallelFor.MyParallelFor(0, N, i =>
+        {
+            C[i] = A[i] * B[i] - A[i] * B[i] + A[i] + A[i] * B[i] - A[i] / A[i] + A[i] / B[i];
+            C[i] = A[i] * B[i] - A[i] * B[i] + A[i];
+            C[i] = A[i] * B[i] - A[i] + 1 - 3 + A[i] / B[i];
+            C[i] = A[i] * B[i] - A[i] * B[i] + A[i];
+        });
+        stopwatch.Stop();
+        Console.WriteLine("Naive parallel loop time in milliseconds: {0}",
+                  stopwatch.ElapsedMilliseconds);
+        stopwatch.Reset();
+
+        stopwatch.Start();
+        Parallel.For(0, N, i =>
+        {
+            C[i] = A[i] * B[i] - A[i] * B[i] + A[i] + A[i] * B[i] - A[i] / A[i] + A[i] / B[i];
+            C[i] = A[i] * B[i] - A[i] * B[i] + A[i];
+            C[i] = A[i] * B[i] - A[i] + 1 - 3 + A[i] / B[i];
+            C[i] = A[i] * B[i] - A[i] * B[i] + A[i];
+        });
+        stopwatch.Stop();
+        Console.WriteLine("Parallel.for loop time in milliseconds: {0}",
+                  stopwatch.ElapsedMilliseconds);
+        stopwatch.Reset();
+
+        abc[] array = new abc[N];
+        for(int i = 0; i < N; i++)
+        {
+            array[i].A = A[i];
+            array[i].B = B[i];
+            array[i].C = C[i];
+        }
+
+        stopwatch.Start();
+        Parallel.ForEach(array, arg =>
+            {
+                arg.C = arg.A * arg.B - arg.A * arg.B + arg.A + arg.A * arg.B - arg.A / arg.A + arg.A / arg.B;
+                arg.C = arg.A * arg.B - arg.A * arg.B + arg.A;
+                arg.C = arg.A * arg.B - arg.A + 1 - 3 + arg.A / arg.B;
+                arg.C = arg.A * arg.B - arg.A * arg.B + arg.A;
+
+            });
+        stopwatch.Stop();
+        Console.WriteLine("Parallel.ForEach loop time in milliseconds: {0}",
+                  stopwatch.ElapsedMilliseconds);
+        stopwatch.Reset();
+        Console.WriteLine("Finished");
+    }
+}
