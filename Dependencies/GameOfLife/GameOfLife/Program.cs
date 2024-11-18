24b50cdfc709731aba8c6847efb8d11a71d0abf5
aleksandrabruska <aleksandrabr405@gmail.com>
2024-11-18 13:32:03 +0100

    1st version - barrier sadly does not work

---
+++ Dependencies/GameOfLife/GameOfLife/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GameOfLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            GameOfLife gol = new GameOfLife(100);
+
+            /*stopwatch.Start();
+            gol.Run(1000);
+            stopwatch.Stop();
+            Console.WriteLine("Serial time " + stopwatch.ElapsedMilliseconds);
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            gol.RunParallel(1000);
+            stopwatch.Stop();
+            Console.WriteLine("Parallel time " + stopwatch.ElapsedMilliseconds);
+            stopwatch.Reset();
+
+            */
+            stopwatch.Start();
+            gol.RunWithBarrier(1000);
+            stopwatch.Stop();
+            Console.WriteLine("Barrier time " + stopwatch.ElapsedMilliseconds);
+            stopwatch.Reset();
+
+
+        }
+    }
+}
